0888288efcbc72fbfd87b655f7e27a18f71ace1e
lepompongraphic <lausanmi87@gmail.com>
2018-05-15 10:42:12 +0200

    Première version de mon projet

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE HTML>
+
+<html  lang="fr" >
+    
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <title>GitHub</title>
+        
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.0/normalize.min.css">
+        
+        <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+        
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+    </head>
+    
+    <body>
+        
+        
+        <script src="js/script.js"></script>
+    </body>
+</html>
